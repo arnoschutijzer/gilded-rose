dc427dbc8b4759cbaf424180054599a191e55371
Arno Schutijzer <arno.schutijzer@protonmail.com>
2025-03-09 22:24:51 +0100

    refactor: extract a method for "normal items"

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -38,6 +38,18 @@ class GildedRose {
         capQualityTo50(item);
     }
 
+    private void updateQualityForANormalItem(Item item) {
+        if (item.quality > 0) {
+            item.quality = item.quality - 1;
+        }
+
+        item.sellIn = item.sellIn - 1;
+
+        if (item.sellIn < 0 && item.quality > 0) {
+            item.quality = item.quality - 1;
+        }
+    }
+
     private void capQualityTo50(Item item) {
         if (item.quality > 50) {
             item.quality = 50;
@@ -46,29 +58,20 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                return;
+            switch (items[i].name) {
+                case "Sulfuras, Hand of Ragnaros":
+                    return;
+                case "Backstage passes to a TAFKAL80ETC concert":
+                    updateQualityForBackstagePassesToATAFKAL80ETCConcert(items[i]);
+                    return;
+                case "Aged Brie":
+                    updateQualityForAgedBrie(items[i]);
+                    return;
+                default:
+                    updateQualityForANormalItem(items[i]);
+                    return;
             }
 
-            if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                updateQualityForBackstagePassesToATAFKAL80ETCConcert(items[i]);
-                return;
-            }
-
-            if (items[i].name.equals("Aged Brie")) {
-                updateQualityForAgedBrie(items[i]);
-                return;
-            }
-
-            if (items[i].quality > 0) {
-                items[i].quality = items[i].quality - 1;
-            }
-
-            items[i].sellIn = items[i].sellIn - 1;
-
-            if (items[i].sellIn < 0 && items[i].quality > 0) {
-                items[i].quality = items[i].quality - 1;
-            }
         }
     }
 }
